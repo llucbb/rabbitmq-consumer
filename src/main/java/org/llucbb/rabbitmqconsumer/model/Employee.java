e27854c8e6c987a346e28a1f94ccadf9db475888
llucbb <llucbb@gmail.com>
2020-03-27 17:17:26 +0100

    Consume JSON custom format message

---
+++ src/main/java/org/llucbb/rabbitmqconsumer/model/Employee.java
@@ -0,0 +1,26 @@
+package org.llucbb.rabbitmqconsumer.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.llucbb.rabbitmqconsumer.json.CustomLocalDateDeserializer;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+public class Employee {
+
+    @JsonProperty("employee_id")
+    private String id;
+
+    private String name;
+
+    @JsonProperty("birth_date")
+    @JsonDeserialize(using = CustomLocalDateDeserializer.class)
+    private LocalDate birthDate;
+
+}
---
+++ src/main/java/org/llucbb/rabbitmqconsumer/service/EmployeeConsumerService.java
@@ -0,0 +1,25 @@
+package org.llucbb.rabbitmqconsumer.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.llucbb.rabbitmqconsumer.model.Employee;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class EmployeeConsumerService {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @RabbitListener(queues = "course.employee")
+    public void listen(String message) {
+        try {
+            var emp = objectMapper.readValue(message, Employee.class);
+            log.info("Employee is {}", emp);
+        } catch (JsonProcessingException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+}
