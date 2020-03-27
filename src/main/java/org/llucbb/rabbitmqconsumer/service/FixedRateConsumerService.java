2dcd9eabb2bd32ac1c7fcdb820d548d753640435
llucbb <llucbb@gmail.com>
2020-03-27 16:28:53 +0100

    Fixed rate concurrent consumers

--- src/main/java/org/llucbb/rabbitmqconsumer/service/FixedRateConsumerService.java
+++ src/main/java/org/llucbb/rabbitmqconsumer/service/FixedRateConsumerService.java
@@ -4,12 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @Service
 @Slf4j
 public class FixedRateConsumerService {
 
-    @RabbitListener(queues = "course.fixedrate")
+    @RabbitListener(queues = "course.fixedrate", concurrency = "3")
     public void listen(String message) {
-        log.info("Consuming {}", message);
+        log.info("Consuming {} on thread {}", message, Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(ThreadLocalRandom.current().nextLong(2000));
+        } catch (InterruptedException e) {
+            log.error(e.getMessage(), e);
+        }
     }
 }
