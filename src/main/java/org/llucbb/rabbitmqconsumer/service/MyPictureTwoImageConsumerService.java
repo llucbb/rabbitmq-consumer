329a8a0b77bd89576bf36984f9901dc16cd59034
llucbb <llucbb@gmail.com>
2020-03-29 17:14:09 +0200

    Moved acknowledge mode to @RabbitListener annotation instead of app config

--- src/main/java/org/llucbb/rabbitmqconsumer/service/MyPictureTwoImageConsumerService.java
+++ src/main/java/org/llucbb/rabbitmqconsumer/service/MyPictureTwoImageConsumerService.java
@@ -20,15 +20,18 @@ public class MyPictureTwoImageConsumerService {
      * Dead letter exchange to handle errors at consumer using org.springframework.amqp.core.Message and
      * com.rabbitmq.client.Channel.
      */
-    @RabbitListener(queues = "q.mypicture.image")
+    @RabbitListener(queues = "q.mypicture.image", ackMode = "MANUAL")
     public void listen(Message message, Channel channel) throws IOException {
         var p = objectMapper.readValue(message.getBody(), Picture.class);
 
         if (p.getSize() > 5000) {
+            // Reject a message. Requeue parameter is true if the rejected message should be requeued rather than
+            // dead-lettered. In that case will be dead-lettered to q.mypicture.dlx queue.
             channel.basicReject(message.getMessageProperties().getDeliveryTag(), false);
         }
 
         log.info("On image : {}", p.toString());
+        // The listener must acknowledge all messages as acknowledge mode has been set to manual
         channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
     }
 }
